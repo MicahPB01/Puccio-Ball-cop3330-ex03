34c59956794727e89cc0a68f565b770c581d4835
MicahPB01 <micahpuccioball@gmail.com>
2021-06-02 17:44:23 -0400

    Updated

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -5,14 +5,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner getQuote = new Scanner(System.in);
-        Scanner getAuthor = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
 
         System.out.print("What is the quote? ");
-        String quote = getQuote.nextLine();
+        String quote = input.nextLine();
 
         System.out.print("Who said it? ");
-        String author = getAuthor.nextLine();
+        String author = input.nextLine();
 
         String output = author + " says \""  + quote + "\"";
 
